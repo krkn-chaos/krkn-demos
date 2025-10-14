85a7f474a0321bcac467eff0af6351bf1582db1a
Tullio Sebastiani <tsebasti@redhat.com>
2025-10-14 10:34:15 +0200

    fix config

--- krknctl/run_app_outage.sh
+++ krknctl/run_app_outage.sh
@@ -7,12 +7,10 @@ export NAMESPACE="$($YQ ".scenarios.app-outage.namespace" $CONFIG)"
 export BLOCK_TRAFFIC_TYPE="$($YQ ".scenarios.app-outage.block-traffic" $CONFIG)"
 export POD_SELECTOR="$($YQ ".scenarios.app-outage.pod-selector" $CONFIG)"
 
-$PODMAN run --name=application_outage --net=host --env-host=true -v $KUBECONFIG:/home/krkn/.kube/config:Z quay.io/krkn-chaos/krkn-hub:application-outages
-
 
 $KRKNCTL run application-outages \
     --kubeconfig ../kubeconfig \
     --chaos-duration $DURATION \
     --namespace $NAMESPACE \
     --block-traffic-type $BLOCK_TRAFFIC_TYPE \
-    --pod-selector $POD_LABEL
+    --pod-selector $POD_SELECTOR
